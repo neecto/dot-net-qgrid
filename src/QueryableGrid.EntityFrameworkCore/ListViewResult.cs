79c75770420b821241ae55b52f0c84d72b92d478
Nick Tarasov <nick.tarasov@cortlex.com>
2021-03-23 14:32:46 +0300

    added QueryableGrid.EntityFrameworkCore project.

---
+++ src/QueryableGrid.EntityFrameworkCore/ListViewResult.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace QueryableGrid.EntityFrameworkCore
+{
+    public class ListViewResult<T> where T : class
+    {
+        public int PageNumber { get; set; }
+        public int ItemsOnPage { get; set; }
+        public int PagesTotal { get; set; }
+        public int Total { get; set; }
+        public int TotalFiltered { get; set; }
+        public IList<T> Items { get; set; }
+    }
+}
---
+++ src/QueryableGrid.EntityFrameworkCore/QueryableExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using QueryableGrid.Extensions;
+using QueryableGrid.Models;
+
+namespace QueryableGrid.EntityFrameworkCore
+{
+    public static class QueryableExtensions
+    {
+        public static async Task<ListViewResult<T>> ToListViewResultAsync<T>(this IQueryable<T> query, ListViewRequest request)
+            where T : class
+        {
+            var total = await query.CountAsync();
+            var resultQuery = query
+                .ApplyFilters(request.Filters)
+                .ApplyOrdering(request.OrderBy, request.ThenOrderBy);
+
+            var totalFiltered = await resultQuery.CountAsync();
+            var skip = request.PageSize * (request.PageNumber - 1);
+
+            var pageResults = await resultQuery
+                .Skip(skip)
+                .Take(request.PageSize)
+                .ToListAsync();
+
+            var pagesTotal = (int)Math.Ceiling((double)totalFiltered / (double)request.PageSize);
+
+            return new ListViewResult<T>
+            {
+                Items = pageResults,
+                ItemsOnPage = pageResults.Count,
+                PageNumber = request.PageNumber,
+                PagesTotal = pagesTotal,
+                Total = total,
+                TotalFiltered = totalFiltered
+            };
+        }
+    }
+}
