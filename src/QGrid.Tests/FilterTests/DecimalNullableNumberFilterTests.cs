6be845b66fe3aa9a9fed4bbf35039031f9636d39
Nick Tarasov <nick.tarasov@cortlex.com>
2021-03-31 17:40:57 +0300

    added DecimalNullableNumberFilterTests.

---
+++ src/QGrid.Tests/FilterTests/DecimalNullableNumberFilterTests.cs
@@ -0,0 +1,153 @@
+﻿using System.Collections.Generic;
+using QGrid.Enums;
+using QGrid.Tests.Fixtures;
+using Xunit;
+
+namespace QGrid.Tests.FilterTests
+{
+    [Collection("Database collection")]
+    public class DecimalNullableNumberFilterTests : BaseFilterTests
+    {
+        public DecimalNullableNumberFilterTests(DatabaseFixture fixture) : base(fixture, "DecimalNullableColumn")
+        {
+        }
+
+        [Fact]
+        public void Int_Eq_HasResults()
+        {
+            var filters = CreateQGridFilters(FilterConditionEnum.Eq, 9.99m);
+
+            var result = ExecuteQuery(filters);
+
+            Assert.NotEmpty(result);
+            Assert.True(result.Count < Fixture.TotalItems);
+            Assert.All(result, x => Assert.Equal(1, x.DecimalNullableColumn));
+            Assert.All(result, x => Assert.NotNull(x.DecimalNullableColumn));
+        }
+
+        [Fact]
+        public void Int_Eq_NoResults()
+        {
+            var filters = CreateQGridFilters(FilterConditionEnum.Eq, 9.98m);
+
+            var result = ExecuteQuery(filters);
+
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public void Int_Neq_HasResults()
+        {
+            var filters = CreateQGridFilters(FilterConditionEnum.Neq, 20.98m);
+
+            var result = ExecuteQuery(filters);
+
+            Assert.NotEmpty(result);
+            Assert.True(result.Count < Fixture.TotalItems);
+            Assert.All(result, x => Assert.NotEqual(20.98m, x.DecimalNullableColumn));
+            Assert.All(result, x => Assert.NotNull(x.DecimalNullableColumn));
+        }
+
+        [Fact]
+        public void Int_Neq_NoResults()
+        {
+            var filters = CreateQGridFilters(FilterConditionEnum.Neq, new List<object> { 9.99m, 21.55m, 20.98m });
+
+            var result = ExecuteQuery(filters);
+
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public void Int_Lt_HasResults()
+        {
+            var filters = CreateQGridFilters(FilterConditionEnum.Lt, 20.98m);
+
+            var result = ExecuteQuery(filters);
+
+            Assert.NotEmpty(result);
+            Assert.True(result.Count < Fixture.TotalItems);
+            Assert.All(result, x => Assert.True(x.DecimalNullableColumn < 20.98m));
+            Assert.All(result, x => Assert.NotNull(x.DecimalNullableColumn));
+        }
+
+        [Fact]
+        public void Int_Lt_NoResults()
+        {
+            var filters = CreateQGridFilters(FilterConditionEnum.Lt, 9.99m);
+
+            var result = ExecuteQuery(filters);
+
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public void Int_Gt_HasResults()
+        {
+            var filters = CreateQGridFilters(FilterConditionEnum.Gt, 20.90m);
+
+            var result = ExecuteQuery(filters);
+
+            Assert.NotEmpty(result);
+            Assert.True(result.Count < Fixture.TotalItems);
+            Assert.All(result, x => Assert.True(x.DecimalNullableColumn > 20.90m));
+            Assert.All(result, x => Assert.NotNull(x.DecimalNullableColumn));
+        }
+
+        [Fact]
+        public void Int_Gt_NoResults()
+        {
+            var filters = CreateQGridFilters(FilterConditionEnum.Gt, 21.55m);
+
+            var result = ExecuteQuery(filters);
+
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public void Int_Lte_HasResults()
+        {
+            var filters = CreateQGridFilters(FilterConditionEnum.Lte, 9.99m);
+
+            var result = ExecuteQuery(filters);
+
+            Assert.NotEmpty(result);
+            Assert.True(result.Count < Fixture.TotalItems);
+            Assert.All(result, x => Assert.True(x.DecimalNullableColumn <= 9.99m));
+            Assert.All(result, x => Assert.NotNull(x.DecimalNullableColumn));
+        }
+
+        [Fact]
+        public void Int_Lte_NoResults()
+        {
+            var filters = CreateQGridFilters(FilterConditionEnum.Lte, 9.97m);
+
+            var result = ExecuteQuery(filters);
+
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public void Int_Gte_HasResults()
+        {
+            var filters = CreateQGridFilters(FilterConditionEnum.Gte, 21.55m);
+
+            var result = ExecuteQuery(filters);
+
+            Assert.NotEmpty(result);
+            Assert.True(result.Count < Fixture.TotalItems);
+            Assert.All(result, x => Assert.True(x.DecimalNullableColumn >= 21.55m));
+            Assert.All(result, x => Assert.NotNull(x.DecimalNullableColumn));
+        }
+
+        [Fact]
+        public void Int_Gte_NoResults()
+        {
+            var filters = CreateQGridFilters(FilterConditionEnum.Gte, 21.56m);
+
+            var result = ExecuteQuery(filters);
+
+            Assert.Empty(result);
+        }
+    }
+}
--- src/QGrid.Tests/Setup/TestDataGenerator.cs
+++ src/QGrid.Tests/Setup/TestDataGenerator.cs
@@ -13,31 +13,37 @@ namespace QGrid.Tests.Setup
                 {
                     IntColumn = 1,
                     IntNullableColumn = null,
-                    DecimalColumn = 1.99m
+                    DecimalColumn = 1.99m,
+                    DecimalNullableColumn = 9.99m
                 },
                 new TestItem
                 {
                     IntColumn = 2,
                     IntNullableColumn = 5,
-                    DecimalColumn = 1.51m
+                    DecimalColumn = 1.51m,
+                    DecimalNullableColumn = null
                 },
                 new TestItem
                 {
                     IntColumn = 10,
                     IntNullableColumn = 6,
-                    DecimalColumn = 1.85m
+                    DecimalColumn = 1.85m,
+                    DecimalNullableColumn = null
+
                 },
                 new TestItem
                 {
                     IntColumn = 20,
                     IntNullableColumn = null,
-                    DecimalColumn = 20.50m
+                    DecimalColumn = 20.50m,
+                    DecimalNullableColumn = 21.55m
                 },
                 new TestItem
                 {
                     IntColumn = 20,
                     IntNullableColumn = 8,
-                    DecimalColumn = 20.99m
+                    DecimalColumn = 20.99m,
+                    DecimalNullableColumn = 20.98m
                 }
             };
 
